13e17ed5df02a025e3e3d2ee970a65115cf7d577
Saranya Govindaraj <findsaranyag@gmail.com>
2024-01-28 10:24:51 +0530

    app routes and config modified

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path:"",
+      loadChildren : () => import("./user/user.routes")
+    },
+    {
+    path : 'admin',
+    loadChildren : () => import("./admin/admin.routes")
+}];
