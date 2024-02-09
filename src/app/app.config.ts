b8bd463ae734788a8bc9ee7e46e8ba153510cab4
Saranya Govindaraj <findsaranyag@gmail.com>
2024-02-09 21:33:18 +0530

    angular material installed

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { coreProviders } from './core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), ...coreProviders,importProvidersFrom(HttpClientModule)],
+  providers: [provideRouter(routes), ...coreProviders, importProvidersFrom(HttpClientModule), provideAnimations()],
 };
